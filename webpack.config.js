4bcd42074163d89ceb6357bc4e70e1b971f97da5
rubencosta <rmc@nubolab.org>
2015-07-29 14:13:30 +0100

    build: separate vendor and app

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
+var webpack = require('webpack');
 var bower_dir = __dirname + '/bower_components';
 
 var config = {
     addVendor: function (name, path) {
         this.resolve.alias[name] = path;
+        this.entry.vendor.push(name);
         if(!(path.indexOf('.css') > -1)){
             this.module.noParse.push(new RegExp(path));
         }
     },
-    entry: ['./src/app/app.js'],
+    entry: {
+        app: ['./src/app/app.js'],
+        vendor: []
+    },
     resolve: {alias: {}},
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js')
+    ],
     output: {
         path: './build',
         filename: 'bundle.js'
     },
     module: {
-        // There is no reason for WebPack to parse this file
         noParse: [],
         loaders: [
             {test: /\.js$/, loader: 'jsx-loader'},
