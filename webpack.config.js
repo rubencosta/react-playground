0d7187fa52f96231d75ef918cbc18a1af9503e50
rubencosta <rmc@nubolab.org>
2015-07-29 10:24:10 +0100

    deps: use bower for app dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
+var bower_dir = __dirname + '/bower_components';
+
 module.exports = {
     entry: ['./src/app/app.js'],
+
+    // The resolve.alias object takes require expressions
+    // (require('react')) as keys and filepath to actual
+    // module as values
+    resolve: {
+        alias: {
+            'react': bower_dir + '/react/react.min.js'
+        }
+    },
     output: {
         path: './build',
         filename: 'bundle.js'
     },
     module: {
+        // There is no reason for WebPack to parse this file
+        noParse: [bower_dir + '/react/react.min.js'],
         loaders: [
             {test: /\.js$/, loader: 'jsx-loader'}
         ]
