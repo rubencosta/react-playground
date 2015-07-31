a8eb9bd2304f59b17862ffc8b895c8083e0f094e
rubencosta <rmc@nubolab.org>
2015-07-31 17:48:03 +0100

    chat: get users from message store

--- src/app/components/mdlLayoutDrawer/MdlLayoutDrawer.js
+++ src/app/components/mdlLayoutDrawer/MdlLayoutDrawer.js
@@ -1,17 +1,40 @@
 'use strict';
 
 var React = require('react');
+var MessageStore = require('../../stores/MessageStore');
 
 module.exports = React.createClass({
+    getInitialState: function () {
+        var allMessages = MessageStore.getAllChats();
+
+        var messageList = [];
+        for (var id in allMessages) {
+            var item = allMessages[id];
+
+            var messagesLength = item.messages.length;
+            messageList.push({
+                lastMessage: item.messages[messagesLength -1],
+                lastAccess: item.lastAccess,
+                user: item.user
+            })
+        }
+
+        return {
+            openChatID: MessageStore.getOpenChatUserID(),
+            messageList: messageList
+        };
+    },
     render: function () {
         return (
             <div className="mdl-layout__drawer">
                 <span className="mdl-layout-title">Title</span>
                 <nav className="mdl-navigation">
-                    <a className="mdl-navigation__link" href="">Link</a>
-                    <a className="mdl-navigation__link" href="">Link</a>
-                    <a className="mdl-navigation__link" href="">Link</a>
-                    <a className="mdl-navigation__link" href="">Link</a>
+                    {
+                        this.state.messageList.map(function (message) {
+                            return <a className="mdl-navigation__link" href="">{message.user}</a>
+
+                        })
+                    }
                 </nav>
             </div>
         )
--- src/app/stores/MessageStore.js
+++ src/app/stores/MessageStore.js
@@ -4,30 +4,6 @@ var Reflux = require('reflux');
 var ChatActions = require('../actions/ChatActions');
 
 var messages = {
-    1: {
-        user: {
-            profilePicture: 'https://avatars0.githubusercontent.com/u/7922109?v=3&s=460',
-            id: 1,
-            name: 'Ryan Clark',
-            status: 'online'
-        },
-        lastAccess: {
-            recipient: 1424469794050,
-            currentUser: 1424469794080
-        },
-        messages: [
-            {
-                contents: 'Hey!',
-                from: 2,
-                timestamp: 1424469793023
-            },
-            {
-                contents: 'Hey, what\'s up?',
-                from: 1,
-                timestamp: 1424469794000
-            }
-        ]
-    },
     2: {
         user: {
             profilePicture: 'https://avatars0.githubusercontent.com/u/7922109?v=3&s=460',
@@ -51,6 +27,45 @@ var messages = {
                 timestamp: 1424469794000
             }
         ]
+    },
+    3: {
+        user: {
+            read: true,
+            profilePicture: 'https://avatars3.githubusercontent.com/u/2955483?v=3&s=460',
+            name: 'Jilles Soeters',
+            id: 3,
+            status: 'online'
+        },
+        lastAccess: {
+            recipient: 1424352522000,
+            currentUser: 1424352522080
+        },
+        messages: [
+            {
+                contents: 'Want a game of ping pong?',
+                from: 3,
+                timestamp: 1424352522000
+            }
+        ]
+    },
+    4: {
+        user: {
+            name: 'Todd Motto',
+            id: 4,
+            profilePicture: 'https://avatars1.githubusercontent.com/u/1655968?v=3&s=460',
+            status: 'online'
+        },
+        lastAccess: {
+            recipient: 1424423579000,
+            currentUser: 1424423574000
+        },
+        messages: [
+            {
+                contents: 'Please follow me on twitter I\'ll pay you',
+                timestamp: 1424423579000,
+                from: 4
+            }
+        ]
     }
 };
 
@@ -66,7 +81,7 @@ module.exports = Reflux.createStore({
     getChatByUserID: function (id) {
         return messages[id];
     },
-    getAll: function () {
+    getAllChats: function () {
         return messages;
     }
 });
