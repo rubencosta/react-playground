ce5cabf319cb1078ab5817cbf1c129c30b70ed84
rubencosta <rmc@nubolab.org>
2015-08-03 14:30:35 +0100

    refactor: user list

--- src/app/components/shiat/shiatUserList/ShiatUserList.js
+++ src/app/components/shiat/shiatUserList/ShiatUserList.js
@@ -42,15 +42,15 @@ module.exports = React.createClass({
         ChatActions.updateOpenChatID(this.state.chatList[index].user.id);
     },
     render: function () {
+        var chats = this.state.chatList.map(function (chat, index) {
+            return <a className="mdl-navigation__link" onClick={this.updateOpenChat.bind(this, index)}>{chat.user.name}<br/> {chat.lastAccess.currentUser}<br/></a>
+        }.bind(this));
+
         return (
             <div className="mdl-layout__drawer">
                 <span className="mdl-layout-title">{this.props.title}</span>
                 <nav className="mdl-navigation">
-                    {
-                        this.state.chatList.map(function (chat, index) {
-                            return <a className="mdl-navigation__link" onClick={this.updateOpenChat.bind(this, index)}>{chat.user.name}<br/> {chat.lastAccess.currentUser}<br/></a>
-                        }.bind(this))
-                    }
+                    {chats}
                 </nav>
             </div>
         )
