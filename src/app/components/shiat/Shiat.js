d95aa8e0b062dac0a64895e9b68f472bd176ba7d
rubencosta <rmc@nubolab.org>
2015-08-03 15:07:32 +0100

    chat: changed layout

--- src/app/components/shiat/Shiat.js
+++ src/app/components/shiat/Shiat.js
@@ -8,8 +8,7 @@ var ShiatMessageBox = require('./shiatMessageBox/ShiatMessageBox');
 module.exports = React.createClass({
     render: function () {
         return (
-            <div className="mdl-layout mdl-js-layout mdl-mdl-layout__drawer-button mdl-layout--fixed-header">
-                <ShiatHeader/>
+            <div className="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--overlay-drawer-button">
                 <ShiatUserList ref="userList" title="Shiats"/>
                 <ShiatMessageBox ref="messageBox"/>
             </div>
